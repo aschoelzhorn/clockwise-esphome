0578211aec443485bc747726f34ab50fc66b3d03
Alexander Schölzhorn <alexander.sch@gmx.at>
2025-11-07 13:52:40 +0100

    dune

--- components/clockwise_hub75/dune_Clockface.h
+++ components/clockwise_hub75/dune_Clockface.h
@@ -10,8 +10,6 @@
 #include "IClockface.h"
 #include "CWDateTime.h"
 
-#include "dune_assets.h"
-
 namespace dune {
 class Clockface : public IClockface {
   private:
@@ -27,13 +25,36 @@ class Clockface : public IClockface {
     static const int BACKGROUND_COUNT = 5;
     static const unsigned long BACKGROUND_CHANGE_INTERVAL = 5000; // 5 seconds
     
-    void drawBackground();
-    void cycleBackground();
+    void drawBackground() {
+      // Simple color cycling for now to ensure linking works
+      uint16_t colors[5] = {0xF800, 0x07E0, 0x001F, 0xFFE0, 0xF81F}; // Red, Green, Blue, Yellow, Magenta
+      uint16_t currentColor = colors[currentBackgroundIndex];
+      Locator::getDisplay()->fillRect(0, 0, 64, 64, currentColor);
+    }
+    
+    void cycleBackground() {
+      currentBackgroundIndex = (currentBackgroundIndex + 1) % BACKGROUND_COUNT;
+      drawBackground();
+    }
 
 public:
-    Clockface(Adafruit_GFX* display);
-    ~Clockface();
-    void setup(CWDateTime* dateTime) override;
-    void update() override;
+    Clockface(Adafruit_GFX* display) : _display(display) {
+      Locator::provide(display);
+    }
+    
+    ~Clockface() = default;
+    
+    void setup(CWDateTime* dateTime) override {
+      lastBackgroundChange = millis();
+      drawBackground();
+    }
+    
+    void update() override {
+      // Update background every 5 seconds
+      if (millis() - lastBackgroundChange >= BACKGROUND_CHANGE_INTERVAL) {
+        cycleBackground();
+        lastBackgroundChange = millis();
+      }
+    }
 };
 }  // namespace dune
