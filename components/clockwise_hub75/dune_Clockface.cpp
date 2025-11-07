2fb583877df72490b81b3ae5820c788e06ad11ff
Alexander Schölzhorn <alexander.sch@gmx.at>
2025-11-07 08:43:05 +0100

    Add Dune clockface

---
+++ components/clockwise_hub75/dune_Clockface.cpp
@@ -0,0 +1,6 @@
+// Minimal implementation - all functionality moved to header for guaranteed linking
+#include "dune_Clockface.h"
+
+namespace dune {
+// Empty implementation file - everything is in header to ensure linking
+}  // namespace dune
