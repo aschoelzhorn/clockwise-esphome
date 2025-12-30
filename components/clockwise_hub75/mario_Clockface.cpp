2a9960c0fe3ada29c2a3efcfd64317fe374466f8
Alexander Schölzhorn <alexander.sch@gmx.at>
2025-12-30 21:10:08 +0100

    FIX LOG

--- components/clockwise_hub75/mario_Clockface.cpp
+++ components/clockwise_hub75/mario_Clockface.cpp
@@ -206,10 +206,10 @@ bool Clockface::shouldBeNightMode() {
 void Clockface::applyNightMode(bool enable) {
   _isNightMode = enable;
   _skyColor = _isNightMode ? SKY_COLOR_NIGHT : SKY_COLOR;
-  ESP_LOGD(name(), "_isNightMode changed to: %s. Changed _skyColor to: %u", _isNightMode ? "true" : "false", _skyColor);
+  ESP_LOGD(TAG, "_isNightMode changed to: %s. Changed _skyColor to: %u", _isNightMode ? "true" : "false", _skyColor);
   // Broadcast new sky color to all listeners
   if (eventBus) {
-    ESP_LOGD(name(), "Broadcasting SKY_COLOR_CHANGED event with color: %u", _skyColor);
+    ESP_LOGD(TAG, "Broadcasting SKY_COLOR_CHANGED event with color: %u", _skyColor);
     eventBus->broadcast(SKY_COLOR_CHANGED, nullptr, _skyColor);
   }
 
