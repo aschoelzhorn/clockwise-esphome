b3fa424046e8af3ceeb76cba5c014d413ab09e0f
Alexander Schölzhorn <alexander.sch@gmx.at>
2026-01-04 12:52:26 +0100

    phrases

--- components/clockwise_hub75/dune_phrases.h
+++ components/clockwise_hub75/dune_phrases.h
@@ -2,35 +2,11 @@
 
 #include <Arduino.h>
 
-const char* PHRASES_TIME[3] = {
-  "TIME FLOWS",
-  "THE HOUR TURNS",
-  "THE MOMENT PASSES"
-};
-
-const char* PHRASES_DESERT[3] = {
-  "THE DESERT WATCHES",
-  "SAND REMEMBERS",
-  "WIND OVER DUNES"
-};
-
-const char* PHRASES_POWER[3] = {
-  "SPICE MUST FLOW",
-  "TIME IS POWER",
-  "ALL EYES OPEN"
-};
-
-const char* PHRASES_DANGER[3] = {
-  "THE MAKER STIRS",
-  "TREAD CAREFULLY",
-  "SILENCE BEFORE STORM"
-};
-
-const char* PHRASES_SURVIVAL[3] = {
-  "ENDURE",
-  "ONLY THE PATIENT",
-  "TIME SURVIVES"
-};
+extern const char* PHRASES_TIME[3];
+extern const char* PHRASES_DESERT[3];
+extern const char* PHRASES_POWER[3];
+extern const char* PHRASES_DANGER[3];
+extern const char* PHRASES_SURVIVAL[3];
 
 
 #define COUNT_TIME      3
