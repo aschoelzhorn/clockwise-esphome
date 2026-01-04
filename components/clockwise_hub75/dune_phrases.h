ee16e76df59a364dcc7af49007aaa353517b595a
Alexander Schölzhorn <alexander.sch@gmx.at>
2026-01-04 19:07:33 +0100

    changes

--- components/clockwise_hub75/dune_phrases.h
+++ components/clockwise_hub75/dune_phrases.h
@@ -2,16 +2,10 @@
 
 #include <Arduino.h>
 
-extern const char* PHRASES_TIME[3];
-extern const char* PHRASES_DESERT[3];
-extern const char* PHRASES_POWER[3];
-extern const char* PHRASES_DANGER[3];
-extern const char* PHRASES_SURVIVAL[3];
+#define COUNT_PHRASES   3
 
-
-#define COUNT_TIME      3
-#define COUNT_DESERT    3
-#define COUNT_POWER     3
-#define COUNT_DANGER    3
-#define COUNT_SURVIVAL  3
-
+extern const char* PHRASES_TIME[COUNT_PHRASES];
+extern const char* PHRASES_DESERT[COUNT_PHRASES];
+extern const char* PHRASES_POWER[COUNT_PHRASES];
+extern const char* PHRASES_DANGER[COUNT_PHRASES];
+extern const char* PHRASES_SURVIVAL[COUNT_PHRASES];
