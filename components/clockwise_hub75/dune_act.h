262a6f36259edc208e3673d87445be8803b3c393
Alexander Schölzhorn <alexander.sch@gmx.at>
2026-01-04 19:17:33 +0100

    ds

---
+++ components/clockwise_hub75/dune_act.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+namespace dune {
+
+class Act {
+
+private:
+    uint8_t _id;
+    const char* _name;
+    const char** _phrases;
+    size_t _phraseCount = COUNT_PHRASES;
+    const uint16_t* _background;
+    int _lastPhraseIndex = -1;
+    uint32_t _lastPhraseUpdate = 0;
+    uint16_t _fontColor;
+
+public:
+    Act(uint8_t id, const char* name, const char** phrases, uint16_t fontColor, const uint16_t* background);
+    uint8_t getId() const;
+    const char* getName() const;
+    const char* getPhrase(size_t index) const;
+    const char* getPhrase() const;
+    size_t getPhraseCount() const;
+    const uint16_t* getBackground() const;
+
+};
+
+}
